70ace22803169972ef73873a4af7a8ceb69760f1
Xavier <{ID}+{username}@users.noreply.github.com>
2026-06-07 09:35:31 +0200

    fix black

--- RMVtransport/rmvtransport.py
+++ RMVtransport/rmvtransport.py
@@ -1,4 +1,5 @@
 """A module to query bus and train departure times."""
+
 import asyncio
 import json
 import logging
--- tests/common.py
+++ tests/common.py
@@ -7,7 +7,7 @@ URL_SEARCH_PATH = "/auskunft/bin/jp/ajax-getstop.exe/dn"
 
 def date_hook(json_dict):
     """JSON datetime parser."""
-    for (key, value) in json_dict.items():
+    for key, value in json_dict.items():
         try:
             json_dict[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
         except (TypeError, ValueError):
